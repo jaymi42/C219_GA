693c44ae2f5ecb8d64cdb4b5f7a52953492302da
jaymi42 <22036150@myrp.edu.sg>
2024-02-02 01:51:29 +0800

    Main code so far

---
+++ booksData.js
@@ -0,0 +1,159 @@
+var booksData = [
+  {
+    id: 0,
+    title: "Atomic Habits",
+    author: "James Clear",
+    genre: "Self-Improvement",
+    year: "2018",
+    type: "Hardcover",
+    image: "img/atomic.jpg",
+    reserved: false,
+    description:
+      "A powerful book on building good habits and breaking bad ones.",
+  },
+  {
+    id: 1,
+    title: "Interesting Facts For Curious Minds",
+    author: "Jordan Moore",
+    genre: "Trivia",
+    year: "1960",
+    type: "Paperback",
+    image: "img/interesting.jpg",
+    reserved: true,
+    description:
+      "A book that contains useful, worthless but undeniably interesting facts that you can impress your friends and family with.",
+  },
+  {
+    id: 2,
+    title: "Elon Musk",
+    author: "Walter Isaacson",
+    genre: "Biography",
+    year: "2023",
+    type: "Hardcover",
+    image: "img/musk.jpg",
+    reserved: false,
+    description:
+      "From the author of Steve Jobs and other bestselling biographies, this is the astonishingly intimate story of the most fascinating and controversial innovator of our era--a rule-breaking visionary who helped to lead the world into the era of electric vehicles, private space exploration, and artificial intelligence.",
+  },
+  {
+    id: 3,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    year: "1925",
+    type: "Hardcover",
+    image: "img/think.jpg",
+    reserved: false,
+    description:
+      "A classic novel that explores the decadence of the Jazz Age and the American Dream.",
+  },
+  {
+    id: 4,
+    title: "Do Androids Dream of Electric Sheep?",
+    author: "Philip K. Dick",
+    genre: "Science Fiction",
+    year: "1968",
+    type: "Paperback",
+    image: "img/sheep.jpg",
+    reserved: false,
+    description:
+      "A science fiction novel set in a post-apocalyptic future where androids are indistinguishable from humans, exploring themes of empathy and identity."
+  },
+  {
+    id: 5,
+    title: "Dune",
+    author: "Frank Herbert",
+    genre: "Science Fiction",
+    year: "1965",
+    type: "Hardcover",
+    image: "img/dune.jpg",
+    reserved: false,
+    description:
+      "A science fiction epic set in a distant future where noble houses vie for control of the desert planet Arrakis, known for its valuable spice melange."
+  },
+  {
+    id: 6,
+    title: "Neuromancer",
+    author: "William Gibson",
+    genre: "Science Fiction",
+    year: "1984",
+    type: "eBook",
+    image: "img/neuro.jpg",
+    reserved: false,
+    description:
+      "A seminal cyberpunk novel that follows a washed-up computer hacker hired to pull off the ultimate hack, blurring the lines between reality and cyberspace."
+  },
+  {
+    id: 7,
+    title: "Foundation",
+    author: "Isaac Asimov",
+    genre: "Science Fiction",
+    year: "1951",
+    type: "Paperback",
+    image: "img/foundation.jpg",
+    reserved: true,
+    description:
+      "The first book in the Foundation series, exploring the collapse and rebirth of a galactic empire through the science of psychohistory."
+  },
+  {
+    id: 8,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fiction",
+    year: "1925",
+    type: "Paperback",
+    image: "img/gatsby.jpg",
+    reserved: false,
+    description:
+      "A classic novel set in the Roaring Twenties, exploring themes of wealth, love, and the American Dream."
+  },
+  {
+    id: 9,
+    title: "The Adventures of Tom Sawyer",
+    author: "Mark Twain",
+    genre: "Adventure",
+    year: "1876",
+    type: "Hardcover",
+    image: "img/sawyer.jpg",
+    reserved: false,
+    description:
+      "A classic adventure novel following the mischievous young boy Tom Sawyer as he navigates life along the Mississippi River."
+  },
+  {
+    id: 10,
+    title: "The Diary of a Young Girl",
+    author: "Anne Frank",
+    genre: "Biography",
+    year: "1947",
+    type: "Paperback",
+    image: "img/annefrank.jpg",
+    reserved: false,
+    description:
+      "The personal diary of Anne Frank, a Jewish girl hiding from the Nazis during World War II, providing a poignant account of life during the Holocaust."
+  },
+  {
+    id: 11,
+    title: "Alice's Adventures in Wonderland",
+    author: "Lewis Carroll",
+    genre: "Fantasy",
+    year: "1865",
+    type: "Hardcover",
+    image: "img/wonderland.jpg",
+    reserved: false,
+    description:
+      "A whimsical fantasy novel following Alice's journey through a fantastical world filled with peculiar characters and nonsensical situations."
+  },
+  {
+    id: 12,
+    title: "Jack Reacher: The Secret",
+    author: "Lee Child",
+    genre: "Thriller",
+    year: "2023",
+    type: "Paperback",
+    image: "img/reacher.jpg",
+    reserved: false,
+    description:
+      "Join Jack Reacher in an electrifying thriller by Lee Child. As Reacher dives into a world of mystery, suspense, and danger, readers are taken on a gripping journey through twists and turns that will keep them hooked until the final page"
+  }
+
+];
---
+++ script.js
@@ -0,0 +1,280 @@
+$(document).ready(function () {
+
+
+  function createBookCard(book) {
+    const isReserved = book.reserved || false;
+
+    return `
+        <div class="col-md-3 mb-3">
+          <div class="card" data-book-id="${book.id
+      }" data-bs-toggle="modal" data-bs-target="#bookModal">
+            <img src="${book.image}" class="card-img-top" alt="${book.title}">
+            <div class="card-body">
+              <h5 class="card-title">${book.title}</h5>
+              <p class="card-text">Author: ${book.author}</p>
+              <button class="btn btn-peach btn-block reserve-btn" type="button" ${isReserved ? "disabled" : ""
+      }>
+                ${isReserved ? "Reserved" : "Reserve"}
+              </button>
+            </div>
+          </div>
+        </div>
+      `;
+  }
+
+
+  function initTooltips() {
+    tippy(".card", {
+      content: "Loading...",
+      onShow(instance) {
+        const bookId = $(instance.reference).data("book-id");
+        const book = booksData.find((book) => book.id === bookId);
+
+        if (book) {
+          const title = $("<strong>").text(book.title);
+          const author = $("<p>").text("Author: " + book.author);
+          const genre = $("<p>").text("Genre: " + book.genre);
+          const year = $("<p>").text("Year: " + book.year);
+          const type = $("<p>").text("Type: " + book.type);
+
+          const desc = $("<p>").text("Description: " + book.description);
+
+          const content = $("<div>").append(title, author, genre, year, type, desc)[0];
+          instance.setContent(content);
+
+          $(instance.reference).on("click", () => {
+            $("#modalBody").html(content);
+          });
+        } else {
+          instance.setContent("Book information is not available...");
+        }
+      },
+    });
+  }
+
+  function renderBookCards() {
+    const bookCardsContainer = $("#bookCardsContainer");
+
+    booksData.forEach((book) => {
+      const bookCardHTML = createBookCard(book);
+      bookCardsContainer.append(bookCardHTML);
+    });
+
+    initTooltips();
+
+    $(".reserve-btn").on("click", function () {
+      const card = $(this).closest(".card");
+      const bookId = card.data("book-id");
+      const book = booksData.find((book) => book.id === bookId);
+
+      if (book && !book.reserved) {
+        $(this).text("Reserved").prop("disabled", true);
+        book.reserved = true;
+        console.log(`Book ${book.title} reserved.`);
+      }
+    });
+  }
+  function filterBookCards(searchInput) {
+    const filteredBooks = booksData.filter((book) => {
+      const searchValue = searchInput.toLowerCase();
+      return (
+        book.title.toLowerCase().includes(searchValue) ||
+        book.author.toLowerCase().includes(searchValue) ||
+        book.genre.toLowerCase().includes(searchValue) ||
+        book.year.toLowerCase().includes(searchValue) ||
+        book.type.toLowerCase().includes(searchValue)
+      );
+    });
+
+    return filteredBooks;
+  }
+
+
+  function reservationChartData() {
+    const genres = [...new Set(booksData.map((book) => book.genre))];
+
+    const genreColors = {
+      "Self-Improvement": "rgba(255, 99, 132, 0.8)",         // Red
+      "Trivia": "rgba(54, 162, 235, 0.8)",                   // Blue
+      "Biography": "rgba(255, 206, 86, 0.8)",                // Yellow
+      "Classic": "rgba(255, 159, 64, 0.8)",                  // Orange
+      "Thriller": "rgba(255, 192, 203, 0.8)",           // Pink
+      "Fantasy": "rgba(75, 192, 192, 0.8)",                  // Teal
+      "Dystopian": "rgba(128, 0, 128, 0.8)",                 // Purple
+      "Philosophical": "rgba(0, 128, 0, 0.8)",               // Green
+      "Science Fiction": "rgba(255, 69, 0, 0.8)",                   // Red-Orange
+      "Fiction": "rgba(255, 0, 255, 0.8)",           // Magenta
+      "Adventure": "rgba(0, 128, 128, 0.8)",               // Teal-Green
+      "Horror": "rgba(128, 0, 0, 0.8)",                      // Dark Red
+    };
+
+    const data = {
+      labels: genres,
+      datasets: [
+        {
+          label: "Books Yet to Be Reserved",
+          data: genres.map((genre) => {
+            const booksInGenre = booksData.filter(
+              (book) => book.genre === genre && !book.reserved
+            );
+            return booksInGenre.length;
+          }),
+          backgroundColor: genres.map((genre) => genreColors[genre] || "rgba(0, 0, 0, 0.8)"),
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    return data;
+  }
+
+  function reservationChart() {
+    const ctx = document.getElementById("reservationChart").getContext("2d");
+
+    new Chart(ctx, {
+      type: "bar",
+      data: reservationChartData(),
+      options: {
+        scales: {
+          x: { title: { display: true, text: "Genre" } },
+          y: { title: { display: true, text: "Number of Books" } },
+        },
+      },
+    });
+  }
+
+  function inventoryTable(data) {
+    $("#inventoryTable").DataTable({
+      data: data,
+      columns: [
+        { data: "title" },
+        { data: "author" },
+        { data: "genre" },
+        { data: "year" },
+        { data: "type" },
+      ],
+      pageLength: 10,
+      lengthMenu: [10, 20, 50],
+      dom: '<"top"lf>rt<"bottom"ip>',
+      lengthChange: false,
+      language: {
+        emptyTable: "No data available in table",
+        info: "Showing _START_ to _END_ of _TOTAL_ entries",
+        infoEmpty: "Showing 0 to 0 of 0 entries",
+        infoFiltered: "(filtered from _MAX_ total entries)",
+        zeroRecords: "No matching records found",
+        search: "Search:",
+        paginate: {
+          previous: "Previous",
+          next: "Next",
+        },
+      },
+      drawCallback: function () {
+        // Apply background color to odd rows after table is drawn
+        $("#inventoryTable tbody tr:odd").css("background-color", "rgb(180, 0, 255)", "color", "white");
+        // Add a class to the title row for custom styling
+        $("#inventoryTable thead tr").addClass("table-violet");
+      },
+    });
+  }
+  // Function to fetch and format data from booksData.js
+  function fetchAndFormatInventoryData() {
+    // Assuming booksData.js is included before app.js
+    if (typeof booksData !== "undefined" && Array.isArray(booksData)) {
+      // Format the data to match the DataTable structure
+      const inventoryData = booksData.map((book) => ({
+        title: book.title,
+        author: book.author,
+        genre: book.genre,
+        year: book.year,
+        type: book.type,
+      }));
+
+      // Call the function to initialize the DataTable with inventoryData
+      inventoryTable(inventoryData);
+    } else {
+      console.error("booksData is not defined or not an array.");
+    }
+  }
+  $(document).ready(function () {
+    fetchAndFormatInventoryData();
+  });
+  $(document).ready(() => {
+    const bookCardsContainer = $("#bookCardsContainer");
+    const maxCards = 6;
+
+    function renderFilteredBooks(filteredBooks) {
+      // Render book cards
+      filteredBooks.forEach((book) => {
+        const bookCardHTML = createBookCard(book);
+        bookCardsContainer.append(bookCardHTML);
+      });
+
+      // Hide or show load more button based on total books
+      if (filteredBooks.length > maxCards) {
+        $("#loadMoreBtn").show();
+      } else {
+        $("#loadMoreBtn").hide();
+      }
+
+      initTooltips();
+
+      $(".reserve-btn").on("click", function () {
+        const card = $(this).closest(".card");
+        const bookId = card.data("book-id");
+        const book = filteredBooks.find((book) => book.id === bookId);
+
+        if (book && !book.reserved) {
+          $(this).text("Reserved").prop("disabled", true);
+          book.reserved = true;
+          console.log(`Book ${book.title} reserved.`);
+        }
+      });
+    }
+
+    renderFilteredBooks(booksData);
+
+    $("#searchInput").on("input", function () {
+      const searchInput = $(this).val();
+      const filteredBooks = filterBookCards(searchInput);
+
+      bookCardsContainer.empty();
+
+      renderFilteredBooks(filteredBooks);
+    });
+
+    $("#loadMoreBtn").on("click", function () {
+      const searchInput = $("#searchInput").val();
+      const filteredBooks = filterBookCards(searchInput);
+
+      const currentCount = bookCardsContainer.children().length;
+      const nextBooks = filteredBooks.slice(
+        currentCount,
+        currentCount + maxCards
+      );
+
+      nextBooks.forEach((book) => {
+        const bookCardHTML = createBookCard(book);
+        bookCardsContainer.append(bookCardHTML);
+      });
+
+      if (filteredBooks.length > currentCount + maxCards) {
+        $("#loadMoreBtn").show();
+      } else {
+        $("#loadMoreBtn").hide();
+      }
+    });
+    reservationChart();
+  });
+
+
+  new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+
+  });
+
+
+
+});
